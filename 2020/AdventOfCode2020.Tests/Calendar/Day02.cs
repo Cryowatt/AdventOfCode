0880b487f173529924671cfd9864a8e01914cb08
Eric Carter <eric.carter@cryowatt.com>
2020-12-02 07:50:29 -0800

    Day 2 Part A

---
+++ 2020/AdventOfCode2020.Tests/Calendar/Day02.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Calendar.Day02
+{
+    public static class BaseTests
+    {
+        public static IEnumerable<(string Input, string Expected)> PartATests => new[]
+        {
+            ("1-3 a: abcde", "1"),
+            ("1-3 b: cdefg", "0"),
+            ("2-9 c: ccccccccc", "1")
+        };
+
+        public static IEnumerable<(string Input, string Expected)> PartBTests => new[]
+        {
+            (@"1721
+            979
+            366
+            299
+            675
+            1456", "241861950")
+        };
+    }
+
+    public class RegularExpressionsTests : DayTest<RegularExpressionsTests, RegularExpressions>
+    {
+        public override IEnumerable<(string Input, string Expected)> PartATests => BaseTests.PartATests;
+
+        public override IEnumerable<(string Input, string Expected)> PartBTests => BaseTests.PartBTests;
+    }
+}
---
+++ 2020/AdventOfCode2020/Calendar/Day02/RegularExpressions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Calendar.Day02
+{
+    public class RegularExpressions : SolutionBase<IEnumerable<(int Min, int Max, char Letter, string Password)>>
+    {
+        private static Regex parser = new(@"^(?<Min>\d+?)\-(?<Max>\d+?) (?<Letter>[a-z]): (?<Password>[a-z]+?)$", RegexOptions.Compiled);
+
+        private static (int Min, int Max, char Letter, string Password) ParseLine(string line)
+        {
+            var match = parser.Match(line);
+            return new(
+                int.Parse(match.Groups["Min"].Value),
+                int.Parse(match.Groups["Max"].Value),
+                match.Groups["Letter"].Value.Single(),
+                match.Groups["Password"].Value
+                );
+        }
+
+        public RegularExpressions() : base(Parsers.Lines(ParseLine)) { }
+
+        public override object PartA() =>
+            this.input.Count(line =>
+            {
+                var check = line.Password.Count(c => c == line.Letter);
+                return line.Min <= check && check <= line.Max;
+            });
+
+        public override object PartB()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
