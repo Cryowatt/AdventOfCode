cfe786837c31894f06a5fae68f233a4eed15f13f
Eric Carter <eric.carter@cryowatt.com>
2020-12-06 17:31:36 -0800

    Day 6 part A

---
+++ 2020/AdventOfCode2020.Tests/Calendar/Day06.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode.Test;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Calendar.Day06
+{
+    public class TestData : ITestData
+    {
+        public IEnumerable<(string Input, string Expected)> PartAData => new[]
+        {
+            (@"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b", "11"),
+        };
+
+        public IEnumerable<(string Input, string Expected)> PartBData => new[]
+        {
+            (@"BFFFBBFRRR
+BFFFBBFRLR", "566"),
+        };
+    }
+
+    public class FunctionalTest : DayTest<Functional, TestData> { }
+}
