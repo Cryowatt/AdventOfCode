294a9c9cb281c186fc63fbc2605c346af5771152
Eric Carter <eric.carter@cryowatt.com>
2020-12-06 17:31:36 -0800

    Day 6 part A

---
+++ 2020/AdventOfCode2020/Calendar/Day06/Functional.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Calendar.Day06
+{
+    public class Functional : SolutionBase<IEnumerable<IEnumerable<string>>>
+    {
+        private static IEnumerable<IEnumerable<string>> Parser(string input)
+        {
+            using (var reader = new StringReader(input))
+            {
+                string line;
+
+                do
+                {
+                    var answers = new List<string>();
+                    while (!string.IsNullOrEmpty(line = reader.ReadLine()))
+                    {
+                        answers.Add(line);
+                    }
+
+                    yield return answers;
+                }
+                while (line != null);
+            }
+        }
+
+        public Functional() : base(Parser) { }
+
+        public override object PartA() =>
+            (from customsGroup in this.input
+             let groupAnswers =
+                 (from response in customsGroup
+                  from answer in response
+                  select answer)
+             select groupAnswers.Distinct().Count()).Sum();
+
+
+        public override object PartB() => "";
+    }
+}
