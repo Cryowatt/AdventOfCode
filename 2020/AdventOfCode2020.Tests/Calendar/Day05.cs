f1e28aee2e8f35a6ac2fc6f316c8964209e5a24d
Eric Carter <eric.carter@cryowatt.com>
2020-12-06 17:20:39 -0800

    Lots of words to describe a binary number

---
+++ 2020/AdventOfCode2020.Tests/Calendar/Day05.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Test;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Calendar.Day05
+{
+    public class TestData : ITestData
+    {
+        public IEnumerable<(string Input, string Expected)> PartAData => new[]
+        {
+            ("BFFFBBFRRR", "567"),
+            ("FFFBBBFRRR", "119"),
+            ("BBFFBBFRLL", "820"),
+        };
+
+        public IEnumerable<(string Input, string Expected)> PartBData => new[]
+        {
+            (@"BFFFBBFRRR
+BFFFBBFRLR", "566"),
+        };
+    }
+
+    public class FunctionalTest : DayTest<Functional, TestData> { }
+}
---
+++ 2020/AdventOfCode2020/Calendar/Day05/Functional.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Calendar.Day05
+{
+    public class Functional : SolutionBase<IEnumerable<string>>
+    {
+        public Functional() : base(Parsers.Lines()) { }
+
+        private int Decode(string pass)
+        {
+            int seatId = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                seatId <<= 1;
+
+                if (pass[i] == 'B' || pass[i] == 'R')
+                {
+                    seatId++;
+                }
+            }
+
+            return seatId;
+        }
+
+        public override object PartA() =>
+            (from pass in this.input
+             select Decode(pass)).Max();
+
+
+        public override object PartB()
+        {
+            var knownSeats =
+                (from pass in this.input
+                 let seatId = Decode(pass)
+                 orderby seatId
+                 select seatId).ToArray();
+
+            for(int i = 0; i < knownSeats.Length; i++)
+            {
+                var seat = knownSeats[i];
+                var nextSeat = knownSeats[i + 1];
+
+                if (seat + 2 == nextSeat)
+                {
+                    return seat + 1;
+                }
+            }
+
+            return "";
+        }
+    }
+}
