b4ba65974b0cd704142b868b0861c61b6404d769
Eric Carter <eric.carter@cryowatt.com>
2020-12-07 21:34:15 -0800

    Late sloppy cpu implementation

---
+++ 2020/AdventOfCode2020.Tests/Calendar/Day08.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCode.Test;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Calendar.Day08
+{
+    public class TestData : ITestData
+    {
+        public IEnumerable<(string Input, string Expected)> PartAData => new[]
+        {
+            (@"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6", "5"),
+        };
+
+        public IEnumerable<(string Input, string Expected)> PartBData => new[]
+        {
+            (@"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6", "8"),
+        };
+
+    public class FunctionalTest : DayTest<Functional, TestData> { }
+    }
+}
