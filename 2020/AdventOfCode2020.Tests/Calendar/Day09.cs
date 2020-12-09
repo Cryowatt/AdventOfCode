4fe6ce670ea166a44b39160c7c2e22e5513c2a2b
Eric Carter <eric.carter@cryowatt.com>
2020-12-09 07:42:46 -0800

    Does not compile yet

---
+++ 2020/AdventOfCode2020.Tests/Calendar/Day09.cs
@@ -0,0 +1,77 @@
+﻿using AdventOfCode.Test;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Calendar.Day09
+{
+    public class TestData : ITestData
+    {
+        string Preamble = @"1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+";
+
+        public IEnumerable<(string Input, string Expected)> PartAData => new[]
+        {
+            (Preamble + "50", "50"),
+            (@"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576", "127")
+        };
+
+        public IEnumerable<(string Input, string Expected)> PartBData => new[]
+        {
+            (@"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6", "8"),
+        };
+
+        public class VectorTest : DayTest<Vector, TestData> { }
+        public class FunctionalTest : DayTest<Functional, TestData> { }
+    }
+}
---
+++ 2020/AdventOfCode2020/Calendar/Day09/Functional.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Calendar.Day09
+{
+    public class Functional : SolutionBase<IEnumerable<int>>
+    {
+        public Functional() : base(Parsers.Lines().Int()) { }
+
+        public override object PartA() => throw new NotImplementedException();
+
+        public override object PartB() => throw new NotImplementedException();
+    }
+}
