5a782f216f9aacb4d2bd6c0ba8337e6ecd2c4fec
Eric Carter <eric.carter@cryowatt.com>
2020-12-12 12:00:01 -0800

    Day 10

---
+++ 2020/AdventOfCode2020.Tests/Calendar/Day10.cs
@@ -0,0 +1,81 @@
+﻿using AdventOfCode.Test;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Calendar.Day10
+{
+    public class TestData : ITestData
+    {
+        public IEnumerable<(string Input, string Expected)> PartAData => new[]
+        {
+            (@"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3", "220"),
+        };
+
+        public IEnumerable<(string Input, string Expected)> PartBData => new[]
+        {
+            (@"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3", "19208"),
+        };
+
+        public class FunctionalTest : DayTest<Functional, TestData> { }
+    }
+}
