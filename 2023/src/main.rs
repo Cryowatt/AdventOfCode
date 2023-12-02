5f6ea19b0c1162537849e1c9f12a9316c5dead7d
Cryowatt <Cryowatt@users.noreply.github.com>
2023-12-01 18:05:00 -0800

    Small cleanup of project structure

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,3 +1,11 @@
+#![feature(test)]
+mod day1;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Running all advent days");
+
+    println!("== Day 1 ==");
+    let input = include_str!("bin/day1/input.txt");
+    println!("Part 1: {}", day1::part1(input));
+    println!("Part 2: {}", day1::part2(input));
 }
