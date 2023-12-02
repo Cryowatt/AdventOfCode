f2ae6f6ff0bcbd0479a252d650b92c29352ee049
Cryowatt <Cryowatt@users.noreply.github.com>
2023-12-02 13:09:09 -0800

    Day 2 part 2

--- 2023/src/day2/mod.rs
+++ 2023/src/day2/mod.rs
@@ -14,90 +14,109 @@ impl Day2 {
     /// assert_eq!(8, advent_of_code::Day2::part1(input));
     /// ```
     pub fn part1(input: &str) -> u32 {
-        let _ = env_logger::builder().is_test(true).try_init();
         static LINE_PARSER: OnceLock<regex::Regex> = OnceLock::new();
         static COLOUR_PARSER: OnceLock<regex::Regex> = OnceLock::new();
 
-        let line_parser = LINE_PARSER.get_or_init(|| {
-            regex::Regex::new(
-                r"Game (\d+): (.*)",
-            )
-            .unwrap()
-        });
-        let colour_parser = COLOUR_PARSER.get_or_init(|| {
-            regex::Regex::new(
-                r"(\d\d+) (red|green|blue)",
-            )
-            .unwrap()
-        });
+        let line_parser =
+            LINE_PARSER.get_or_init(|| regex::Regex::new(r"Game (\d+): (.*)").unwrap());
+        let colour_parser =
+            COLOUR_PARSER.get_or_init(|| regex::Regex::new(r"(\d\d+) (red|green|blue)").unwrap());
 
-        input.lines().filter_map(|line| {
-            let (_, [id, rounds]) = line_parser.captures(line).unwrap().extract();
+        input
+            .lines()
+            .filter_map(|line| {
+                let (_, [id, rounds]) = line_parser.captures(line).unwrap().extract();
 
-            let is_valid = colour_parser.captures_iter(rounds).all(|pull| {
-                let (_, [count, colour]) = pull.extract();
-                match colour {
-                    "red" => count.parse::<u8>().unwrap() <= 12,
-                    "green" => count.parse::<u8>().unwrap() <= 13,
-                    "blue" => count.parse::<u8>().unwrap() <= 14,
-                    _ => unreachable!()
+                let is_valid = colour_parser.captures_iter(rounds).all(|pull| {
+                    let (_, [count, colour]) = pull.extract();
+                    match colour {
+                        "red" => count.parse::<u8>().unwrap() <= 12,
+                        "green" => count.parse::<u8>().unwrap() <= 13,
+                        "blue" => count.parse::<u8>().unwrap() <= 14,
+                        _ => unreachable!(),
+                    }
+                });
+
+                match is_valid {
+                    true => Some(id.parse::<u32>().unwrap()),
+                    false => None,
                 }
-            });
-
-            match is_valid {
-                true => Some(id.parse::<u32>().unwrap()),
-                false => None
-            }
-        }).sum()
+            })
+            .sum()
     }
 
-    
+    /// ```rust
+    /// let input = r"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    /// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    /// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    /// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    /// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    /// assert_eq!(2286, advent_of_code::Day2::part2(input));
+    /// ```
     pub fn part2(input: &str) -> u32 {
-        unimplemented!()
-    }
-}
+        static COLOUR_PARSER: OnceLock<regex::Regex> = OnceLock::new();
 
-#[cfg(test)]
-mod unittests {
-    use crate::Day1;
+        let colour_parser =
+            COLOUR_PARSER.get_or_init(|| regex::Regex::new(r"(\d+) (red|green|blue)").unwrap());
 
-    #[ctor::ctor]
-    fn init() {
-        let _ = env_logger::builder().is_test(true).try_init();
-    }
+        struct DiceCount {
+            red: u32,
+            green: u32,
+            blue: u32,
+        }
 
-    #[test]
-    fn part1_example_test() {
-        let input = r"1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet";
-        assert_eq!(142, Day1::part1(input));
-    }
+        impl DiceCount {
+            const ZERO: DiceCount = DiceCount {
+                red: 0,
+                green: 0,
+                blue: 0,
+            };
+            fn power(&self) -> u32 {
+                self.red * self.green * self.blue
+            }
 
-    #[test]
-    fn part2_example_test() {
-        let input = r"two1nine
-        eightwothree
-        abcone2threexyz
-        xtwone3four
-        4nineeightseven2
-        zoneight234
-        7pqrstsixteen";
-        assert_eq!(281, Day1::part2(input));
-    }
+            fn max_red(&self, red: u32) -> DiceCount {
+                DiceCount {
+                    red: self.red.max(red),
+                    green: self.green,
+                    blue: self.blue,
+                }
+            }
+            fn max_green(&self, green: u32) -> DiceCount {
+                DiceCount {
+                    red: self.red,
+                    green: self.green.max(green),
+                    blue: self.blue,
+                }
+            }
+            fn max_blue(&self, blue: u32) -> DiceCount {
+                DiceCount {
+                    red: self.red,
+                    green: self.green,
+                    blue: self.blue.max(blue),
+                }
+            }
+        }
 
-    #[test]
-    fn part2_overlap_case() {
-        let input = r"two1nine
-    eightwothree
-    abcone2threexyz
-    xtwone3four
-    4nineeightseven2
-    zoneight234
-    7pqrstsixteen
-    eighthree";
-        assert_eq!(281 + 83, Day1::part2(input));
+        input
+            .lines()
+            .map(|line| {
+                let min_dice_set =
+                    colour_parser
+                        .captures_iter(line)
+                        .fold(DiceCount::ZERO, |acc, capture| {
+                            let (_, [count, colour]) = capture.extract();
+                            match colour {
+                                "red" => acc.max_red(count.parse::<u32>().unwrap()),
+                                "green" => acc.max_green(count.parse::<u32>().unwrap()),
+                                "blue" => acc.max_blue(count.parse::<u32>().unwrap()),
+                                _ => unreachable!(),
+                            }
+                        });
+
+                min_dice_set.power()
+            })
+            .sum()
     }
 }
 
