c1a49594b4344a749e304937dcd7a408e2b9985c
Cryowatt <Cryowatt@users.noreply.github.com>
2023-12-01 19:48:55 -0800

    Other reorganizations

--- 2023/src/lib.rs
+++ 2023/src/lib.rs
@@ -1,3 +1,13 @@
 #![feature(test)]
 
-pub mod day1;
+mod day1;
+pub use day1::*;
+
+#[macro_export]
+macro_rules! day {
+    ($id:literal, $day:path) => {        
+        println!("== Day {} ==", $id);
+        println!("Part 1: {}", <$day>::part1(<$day>::INPUT));
+        println!("Part 2: {}", <$day>::part2(<$day>::INPUT));
+    };
+}
