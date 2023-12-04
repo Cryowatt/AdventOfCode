eff1697e406b14d6b27afc8bfb49cba45f05ae81
Cryowatt <Cryowatt@users.noreply.github.com>
2023-12-04 08:33:25 -0800

    Day 4 input and stub

--- 2023/src/bin/day.rs
+++ 2023/src/bin/day.rs
@@ -10,6 +10,7 @@ fn main() {
         1 => { run_day!(1, advent_of_code::Day1); },
         2 => { run_day!(2, advent_of_code::Day2); },
         3 => { run_day!(3, advent_of_code::Day3); },
+        4 => { run_day!(4, advent_of_code::Day4); },
         _ => unimplemented!(),
     }
 }
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -7,5 +7,6 @@ fn main() {
 
     run_day!(1, advent_of_code::Day1);
     run_day!(2, advent_of_code::Day2);
-    run_day!(2, advent_of_code::Day3);
+    run_day!(3, advent_of_code::Day3);
+    run_day!(4, advent_of_code::Day4);
 }
