8726bdcc8aeccb3fbcea7af85157e0638f70cb60
Cryowatt <Cryowatt@users.noreply.github.com>
2023-12-04 08:33:25 -0800

    Day 4 input and stub

---
+++ 2023/src/day4/mod.rs
@@ -0,0 +1,31 @@
+use crate::advent_day;
+
+advent_day!(Day4, part1, part2);
+
+/// ```rust
+/// use advent_of_code::day4::*;
+/// let input = r"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+/// Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+/// Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+/// Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+/// Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+/// Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+/// assert_eq!(13, part1(input));
+/// ```
+pub fn part1(_input: &str) -> u32 {
+    unimplemented!()
+}
+
+/// ```rust
+/// use advent_of_code::day4:*;
+/// let input = r"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+/// Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+/// Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+/// Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+/// Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+/// Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+/// //assert_eq!(467835, part2(input));
+/// ```
+pub fn part2(_input: &str) -> u32 {
+    unimplemented!()
+}
--- 2023/src/lib.rs
+++ 2023/src/lib.rs
@@ -5,10 +5,12 @@
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
 pub use day1::Day1;
 pub use day2::Day2;
 pub use day3::Day3;
+pub use day4::Day4;
 
 #[derive(Copy, Clone)]
 struct UPoint {
