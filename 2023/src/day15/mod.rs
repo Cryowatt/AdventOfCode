268af973f611c55cea000c4acb0efc37b7c0bd45
Cryowatt <Cryowatt@users.noreply.github.com>
2023-12-14 22:43:23 -0800

    Day 15 part 2

--- 2023/src/day15/mod.rs
+++ 2023/src/day15/mod.rs
@@ -3,14 +3,54 @@ use std::{
     collections::{hash_map::DefaultHasher, HashMap},
     hash::Hasher,
     ops::Range,
+    str::Bytes,
 };
 
 use advent::*;
 
-advent_day!(Day15, parse, Vec<&str>, part1, part2);
+advent_day!(Day15, parse, Initialization, part1, part2);
 
-pub fn parse(input: &str) -> Vec<&str> {
-    input.lines().flat_map(|line| line.split(",")).collect()
+pub fn parse(input: &str) -> Initialization {
+    Initialization {
+        hash_test: input.lines().flat_map(|line| line.split(",")).collect(),
+        instructions: input
+            .lines()
+            .flat_map(|line| {
+                line.split(",").map(|item| {
+                        let (label, operation) = match item.chars().last().unwrap() {
+                            '-' => (&item[..item.len() - 1], Operation::Remove),
+                            _ => (
+                                &item[..item.len() - 2],
+                                Operation::Add(
+                                    item.chars().last().unwrap().to_digit(10).unwrap() as u8
+                                ),
+                            ),
+                        };
+                        Instruction {
+                            label,
+                            hash: hash(label.bytes()),
+                            operation,
+                        }
+                    })
+            })
+            .collect(),
+    }
+}
+
+pub struct Initialization<'a> {
+    hash_test: Vec<&'a str>,
+    instructions: Vec<Instruction<'a>>,
+}
+
+struct Instruction<'a> {
+    label: &'a str,
+    hash: u8,
+    operation: Operation,
+}
+
+enum Operation {
+    Remove,
+    Add(u8),
 }
 
 /// ```rust
@@ -73,31 +113,321 @@ pub fn parse(input: &str) -> Vec<&str> {
 /// let input = parse("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7");
 /// assert_eq!(1320, part1(&input));
 /// ```
-pub fn part1(input: &Vec<&str>) -> u32 {
+pub fn part1(input: &Initialization) -> u32 {
     input
+        .hash_test
         .iter()
-        .map(|item| {
-            item.bytes()
-                .fold(0u8, |hash, b| hash.wrapping_add(b).wrapping_mul(17)) as u32
-        })
+        .map(|item| hash(item.bytes()) as u32)
         .sum()
+}
+
+fn hash(bytes: Bytes) -> u8 {
+    bytes.fold(0u8, |hash, b| hash.wrapping_add(b).wrapping_mul(17))
 }
 
 /// ```rust
 /// use advent_of_code_2023::day15::*;
-/// let input = parse(
-/// "O....#....
-/// O.OO#....#
-/// .....##...
-/// OO.#O....O
-/// .O.....O#.
-/// O.#..O.#.#
-/// ..O..#O..O
-/// .......O..
-///##....###..
-///##OO..#....");
-/// //assert_eq!(64, part2(&input));
+/// let input = parse("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7");
+/// assert_eq!(145, part2(&input));
 /// ```
-pub fn part2(input: &Vec<&str>) -> u32 {
-    0
+pub fn part2(input: &Initialization) -> u32 {
+    let mut boxes: [Vec<(&str, u8)>; 256] = [
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+    ];
+
+    fn find_index(hash_box: &Vec<(&str, u8)>, label: &str) -> Option<usize> {
+        for i in 0..hash_box.len() {
+            if hash_box[i].0 == label {
+                return Some(i);
+            }
+        }
+
+        None
+    }
+
+    for instruction in input.instructions.iter() {
+        match instruction.operation {
+            Operation::Remove => {
+                let hash_box = &mut boxes[instruction.hash as usize];
+                if let Some(index) = find_index(&hash_box, instruction.label) {
+                    hash_box.remove(index);
+                }
+            }
+            Operation::Add(lens) => {
+                let hash_box = &mut boxes[instruction.hash as usize];
+                match find_index(&hash_box, instruction.label) {
+                    Some(index) => hash_box[index] = (instruction.label, lens),
+                    None => hash_box.push((instruction.label, lens)),
+                }
+            }
+        }
+    }
+
+    boxes
+        .iter()
+        .enumerate()
+        .map(|(box_id, hash_box)| {
+            hash_box
+                .iter()
+                .enumerate()
+                .map(|(lens_index, (_label, lens))| (lens_index + 1) as u32 * *lens as u32)
+                .sum::<u32>()
+                * (box_id as u32 + 1)
+        })
+        .sum::<u32>()
 }
