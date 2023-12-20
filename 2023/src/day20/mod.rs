5301002b7d60d99b19f6ba97b9fd74cbc2e71e81
Cryowatt <Cryowatt@users.noreply.github.com>
2023-12-19 20:06:40 -0800

    Day 20 stubs

---
+++ 2023/src/day20/mod.rs
@@ -0,0 +1,28 @@
+use advent::*;
+
+advent_day!(Day20, parse, &str, part1, part2);
+
+pub fn parse(input: &str) -> &str {
+    input
+}
+
+/// ```rust
+/// use advent_of_code_2023::day20::*;
+/// let input = parse(
+/// r"
+/// ");
+/// //assert_eq!(?, part1(&input));
+/// ```
+pub fn part1(_input: &str) -> u32 {
+    0s
+}
+
+/// ```rust
+/// use advent_of_code_2023::day20::*;
+/// let input = parse(
+/// r"");
+/// //assert_eq!(?, part2(&input));
+/// ```
+pub fn part2(_input: &str) -> u64 {
+    0
+}
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -25,4 +25,5 @@ fn main() {
     run_day!(17, day17::Day17);
     run_day!(18, day18::Day18);
     run_day!(19, day19::Day19);
+    run_day!(20, day20::Day20);
 }
