208b0152ae4402bc221127b69e8fa8394db6f5b2
Cryowatt <Cryowatt@users.noreply.github.com>
2024-12-03 08:37:38 -0800

    2024 Day 3 Part 2

--- 2024/src/day03/mod.rs
+++ 2024/src/day03/mod.rs
@@ -29,9 +29,33 @@ pub fn part1(input: &str) -> i32 {
 /// ```rust
 /// use advent_of_code_2024::day03::*;
 /// let input = parse(
-/// r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
-/// assert_eq!(0, part2(&input));
+/// r"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
+/// assert_eq!(48, part2(&input));
 /// ```
 pub fn part2(input: &str) -> i32 {
-    0
+    let pattern = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)").unwrap();
+
+    pattern
+        .captures_iter(input)
+        .scan(true, |enabled, capture| {
+            if let Some(_) = capture.get(3) {
+                // Do
+                *enabled = true;
+                Some(0)
+            } else if let Some(_) = capture.get(4) {
+                // Don't
+                *enabled = false;
+                Some(0)
+            } else {
+                if *enabled {
+                    Some(
+                        capture.get(1).unwrap().as_str().parse::<i32>().unwrap()
+                            * capture.get(2).unwrap().as_str().parse::<i32>().unwrap(),
+                    )
+                } else {
+                    Some(0)
+                }
+            }
+        })
+        .sum()
 }
