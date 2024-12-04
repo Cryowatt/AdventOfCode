eac514be844388009491ce287335f9f224932c87
Cryowatt <Cryowatt@users.noreply.github.com>
2024-12-04 13:08:26 -0800

    2024 Day 4 Part 1

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     run_day!(1, day01::Day01);
     run_day!(2, day02::Day02);
     run_day!(3, day03::Day03);
-    // run_day!(4, day04::Day04);
+    run_day!(4, day04::Day04);
     // run_day!(5, day05::Day05);
     // run_day!(6, day06::Day06);
     // run_day!(7, day07::Day07);
