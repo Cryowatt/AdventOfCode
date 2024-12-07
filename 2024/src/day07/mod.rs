8bb1cf3dfa8629d42d6d216654e068a5ceaf3e43
Cryowatt <Cryowatt@users.noreply.github.com>
2024-12-07 10:34:50 -0800

    2024 Day 7 Part 2

--- 2024/src/day07/mod.rs
+++ 2024/src/day07/mod.rs
@@ -33,26 +33,26 @@ pub fn parse(input: &str) -> InputType {
 /// assert_eq!(3749, part1(&input));
 /// ```
 pub fn part1(input: &InputType) -> i64 {
+    fn is_solvable(target: i64, terms: &Vec<i64>) -> bool {
+        fn solve(accumulator: i64, terms: &[i64], target: i64) -> bool {
+            if let Some((term, tail_terms)) = terms.split_first() {
+                let product = accumulator * term;
+                let sum = accumulator + term;
+                (product <= target && solve(product, tail_terms, target))
+                    || (sum <= target && solve(sum, tail_terms, target))
+            } else {
+                target == accumulator
+            }
+        }
+
+        let (term, tail_terms) = terms.split_first().unwrap();
+        solve(*term, tail_terms, target)
+    }
+
     input
         .iter()
         .filter_map(|row| is_solvable(row.0, &row.1).then_some(row.0))
         .sum()
-}
-
-fn is_solvable(target: i64, terms: &Vec<i64>) -> bool {
-    fn solve(accumulator: i64, terms: &[i64], target: i64) -> bool {
-        if let Some((term, tail_terms)) = terms.split_first() {
-            let product = accumulator * term;
-            let sum = accumulator + term;
-            (product <= target && solve(product, tail_terms, target))
-                || (sum <= target && solve(sum, tail_terms, target))
-        } else {
-            target == accumulator
-        }
-    }
-
-    let (term, tail_terms) = terms.split_first().unwrap();
-    solve(*term, tail_terms, target)
 }
 
 /// ```rust
@@ -67,8 +67,34 @@ fn is_solvable(target: i64, terms: &Vec<i64>) -> bool {
 /// 192: 17 8 14
 /// 21037: 9 7 18 13
 /// 292: 11 6 16 20");
-/// assert_eq!(0, part2(&input));
+/// assert_eq!(11387, part2(&input));
 /// ```
-pub fn part2(input: &InputType) -> i32 {
-    0
+pub fn part2(input: &InputType) -> i64 {
+    fn concat(left: i64, right: i64) -> i64 {
+        left * 10i64.pow(right.ilog10() + 1) + right
+    }
+
+    fn is_solvable(target: i64, terms: &Vec<i64>) -> bool {
+        fn solve(accumulator: i64, terms: &[i64], target: i64) -> bool {
+            if let Some((term, tail_terms)) = terms.split_first() {
+                let product = accumulator * term;
+                let sum = accumulator + term;
+                let concat = concat(accumulator, *term);
+
+                (concat <= target && solve(concat, tail_terms, target))
+                    || (product <= target && solve(product, tail_terms, target))
+                    || (sum <= target && solve(sum, tail_terms, target))
+            } else {
+                target == accumulator
+            }
+        }
+
+        let (term, tail_terms) = terms.split_first().unwrap();
+        solve(*term, tail_terms, target)
+    }
+
+    input
+        .iter()
+        .filter_map(|row| is_solvable(row.0, &row.1).then_some(row.0))
+        .sum()
 }
