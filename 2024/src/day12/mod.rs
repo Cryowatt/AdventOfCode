f2d6b4a481854560095dc569453e833cb5a17003
Cryowatt <Cryowatt@users.noreply.github.com>
2024-12-12 12:00:17 -0800

    2024 Day 12 Part 1

--- 2024/src/day12/mod.rs
+++ 2024/src/day12/mod.rs
@@ -1,7 +1,4 @@
 use advent::*;
-use num::{traits::bounds, Integer};
-use rayon::prelude::*;
-use std::collections::HashMap;
 
 advent_day!(Day12, parse, Vec<Vec<u8>>, part1, part2);
 
@@ -29,65 +26,90 @@ pub fn parse(input: &str) -> InputType {
 /// ```
 pub fn part1(input: &InputType) -> u32 {
     let bounds = UPoint::new(input.first().unwrap().len() as u32, input.len() as u32);
-    let mut region_area = [0u32; 26];
-    let mut region_perimeter = [0u32; 26];
+    let visited = &mut (0..bounds.y)
+        .map(|_| (0..bounds.y).map(|_| false).collect())
+        .collect::<Vec<Vec<_>>>();
 
-    let points = (0..bounds.y)
-        .flat_map(|y| (0..bounds.x).map(move |x| UPoint::new(x, y)))
-        .for_each(|position| {
-            let (region, perimeter) = measure(position, input, &bounds);
-            region_area[region as usize] += 1;
-            region_perimeter[region as usize] += perimeter;
-        });
+    fn measure(
+        start: UPoint,
+        map: &InputType,
+        visited: &mut Vec<Vec<bool>>,
+        bounds: &UPoint,
+    ) -> Option<u32> {
+        let current_region = map[start.y as usize][start.x as usize];
+        if current_region == 0xff {
+            None
+        } else {
+            let mut area = 0;
+            let mut perimeter = 0;
+            let mut pending = vec![start];
 
-    fn measure(position: UPoint, map: &InputType, bounds: &UPoint) -> (u8, u32) {
-        let current_region = map[position.y as usize][position.x as usize];
-        let mut perimeter = 0;
+            fn check_adjacent(
+                position: Point<u32>,
+                direction: Direction,
+                bounds: &UPoint,
+                map: &Vec<Vec<u8>>,
+                current_region: u8,
+            ) -> Option<UPoint> {
+                if let Some(adjacent) = position.direction_checked(direction, bounds) {
+                    let adjacent_region = map[adjacent.y as usize][adjacent.x as usize];
+                    if adjacent_region == current_region {
+                        Some(adjacent)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            }
 
-        if let Some(adjacent) = position.north_checked() {
-            if map[adjacent.y as usize][adjacent.x as usize] != current_region {
-                perimeter += 1;
-            }
-        } else {
-            perimeter += 1;
-        }
-        if let Some(adjacent) = position.west_checked() {
-            if map[adjacent.y as usize][adjacent.x as usize] != current_region {
-                perimeter += 1;
-            }
-        } else {
-            perimeter += 1;
-        }
-        if let Some(adjacent) = position.south_checked(bounds) {
-            if map[adjacent.y as usize][adjacent.x as usize] != current_region {
-                perimeter += 1;
-            }
-        } else {
-            perimeter += 1;
-        }
-        if let Some(adjacent) = position.east_checked(bounds) {
-            if map[adjacent.y as usize][adjacent.x as usize] != current_region {
-                perimeter += 1;
-            }
-        } else {
-            perimeter += 1;
-        }
+            while let Some(position) = pending.pop() {
+                if visited[position.y as usize][position.x as usize] {
+                    continue;
+                }
+                visited[position.y as usize][position.x as usize] = true;
+                area += 1;
 
-        (current_region - b'A', perimeter)
+                if let Some(adjacent) =
+                    check_adjacent(position, Direction::North, &bounds, map, current_region)
+                {
+                    pending.push(adjacent);
+                } else {
+                    perimeter += 1;
+                }
+
+                if let Some(adjacent) =
+                    check_adjacent(position, Direction::East, &bounds, map, current_region)
+                {
+                    pending.push(adjacent);
+                } else {
+                    perimeter += 1;
+                }
+
+                if let Some(adjacent) =
+                    check_adjacent(position, Direction::South, &bounds, map, current_region)
+                {
+                    pending.push(adjacent);
+                } else {
+                    perimeter += 1;
+                }
+
+                if let Some(adjacent) =
+                    check_adjacent(position, Direction::West, &bounds, map, current_region)
+                {
+                    pending.push(adjacent);
+                } else {
+                    perimeter += 1;
+                }
+            }
+
+            Some(area * perimeter)
+        }
     }
 
-    (0..26)
-        .map(|region| {
-            let price = region_area[region] * region_perimeter[region];
-            println!(
-                "A region of {} plants with price {} * {} = {}.",
-                (region as u8 + b'A') as char,
-                region_area[region],
-                region_perimeter[region],
-                price
-            );
-            price
-        })
+    (0..bounds.y)
+        .flat_map(|y| (0..bounds.x).map(move |x| UPoint::new(x, y)))
+        .filter_map(|position| measure(position, input, visited, &bounds))
         .sum()
 }
 
@@ -104,7 +126,7 @@ pub fn part1(input: &InputType) -> u32 {
 /// MIIIIIJJEE
 /// MIIISIJEEE
 /// MMMISSJEEE");
-/// assert_eq!(0, part2(&input));
+/// assert_eq!(1206, part2(&input));
 /// ```
 pub fn part2(input: &InputType) -> u64 {
     0
