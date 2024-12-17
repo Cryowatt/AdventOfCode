5cac714af3b583c2469a8d4b40d0f6efe20cc0bc
Cryowatt <Cryowatt@users.noreply.github.com>
2024-12-17 10:14:41 -0800

    2024 Day 17 Stub

--- 2024/src/bin/day.rs
+++ 2024/src/bin/day.rs
@@ -60,9 +60,9 @@ fn main() {
         16 => {
             run_day!(16, day16::Day16);
         }
-        // 17 => {
-        //     run_day!(17, day17::Day17);
-        // }
+        17 => {
+            run_day!(17, day17::Day17);
+        }
         // 18 => {
         //     run_day!(18, day18::Day18);
         // }
