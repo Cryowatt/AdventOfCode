9b1f44b5ba96d9d80060e892a9d6226aaa492b3c
Cryowatt <Cryowatt@users.noreply.github.com>
2024-12-20 23:11:17 -0800

    Build for 2015

---
+++ 2015/build.rs
@@ -0,0 +1,27 @@
+use std::{
+    fs::{copy, read_dir},
+    path::PathBuf,
+};
+
+fn main() -> std::io::Result<()> {
+    for dir in read_dir("../AdventOfCodeInput/2015")
+        .expect("Could not load input files from ../AdventOfCodeInput/2015")
+    {
+        if let Ok(dir) = dir {
+            let source_path = dir.path();
+            let target_path = PathBuf::from(format!(
+                "./src/{}/input.txt",
+                source_path
+                    .with_extension("")
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+            ));
+            println!("{:?} -> {:?}", source_path, target_path);
+            copy(source_path, target_path)?;
+        }
+    }
+
+    Ok(())
+}
