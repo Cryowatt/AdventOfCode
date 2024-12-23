c6cda4665f741f16062fe3cba66e2c9cceefe549
Cryowatt <Cryowatt@users.noreply.github.com>
2024-12-23 12:56:35 -0800

    2024 Day 23 Part 2

--- 2024/src/day23/mod.rs
+++ 2024/src/day23/mod.rs
@@ -133,8 +133,58 @@ pub fn part1(input: &InputType) -> usize {
 /// wh-qp
 /// tb-vc
 /// td-yn");
-/// assert_eq!(0, part2(&input));
+/// assert_eq!("co,de,ka,ta", part2(&input));
 /// ```
-pub fn part2(_input: &InputType) -> i32 {
-    0
+pub fn part2(input: &InputType) -> String {
+    let mut edges: HashMap<&str, HashSet<&str>> = HashMap::new();
+    let mut all_nodes = HashSet::new();
+
+    for (node_a, node_b) in input.iter() {
+        all_nodes.insert(node_a);
+        all_nodes.insert(node_b);
+
+        if let Some(edge_set) = edges.get_mut(node_a) {
+            edge_set.insert(node_b);
+        } else {
+            edges.insert(node_a, HashSet::from([*node_b]));
+        }
+
+        if let Some(edge_set) = edges.get_mut(node_b) {
+            edge_set.insert(node_a);
+        } else {
+            edges.insert(node_b, HashSet::from([*node_a]));
+        }
+    }
+
+    let mut lan_party = HashSet::new();
+    for &node in all_nodes {
+        thing(&mut HashSet::new(), node, &edges, &mut lan_party);
+    }
+
+    fn thing<'a>(
+        network: &mut HashSet<&'a str>,
+        node: &'a str,
+        edges: &HashMap<&str, HashSet<&'a str>>,
+        lan_party: &mut HashSet<&'a str>,
+    ) {
+        let pairs: &HashSet<&str> = edges.get(node).unwrap();
+
+        if pairs.is_superset(&network) {
+            network.insert(node);
+
+            if network.len() > lan_party.len() {
+                *lan_party = network.clone();
+            }
+
+            for &pair_node in pairs.iter().filter(|pair_node| node < pair_node) {
+                thing(network, pair_node, edges, lan_party);
+            }
+            network.remove(node);
+        }
+    }
+
+    let mut canonical_party = Vec::from_iter(lan_party);
+    canonical_party.sort();
+
+    canonical_party.join(",").to_owned()
 }
