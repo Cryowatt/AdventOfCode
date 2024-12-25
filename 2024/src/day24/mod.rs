f2fdff4d8220f79adbe77a62a306232295e89a74
Cryowatt <Cryowatt@users.noreply.github.com>
2024-12-24 20:50:29 -0800

    Blerg

--- 2024/src/day24/mod.rs
+++ 2024/src/day24/mod.rs
@@ -217,17 +217,28 @@ pub fn part1(input: &InputType) -> u64 {
     number
 }
 
+pub fn shuffle_attempt() -> bool {
+    let input = parse(&INPUT);
+    let swaps = part2(&input).split(',').collect::<Vec<_>>();
+
+    // for
+
+    // false
+    true
+}
+
 /// ```rust
 /// use advent_of_code_2024::day24::*;
-/// let input = parse(advent_of_code_2024::day24::INPUT);
-///
-/// // assert_eq!("z00,z01,z02,z05", part2(&input));
+/// assert_eq!(true, shuffle_attempt());
 /// ```
 pub fn part2(input: &InputType) -> String {
     println!();
     println!();
     println!();
     let (_init, nodes) = input;
+    // Expectations:
+    // x + y into AND and XOR
+    // XOR -> z
 
     let dependency_lookup: &mut HashMap<&str, HashSet<&OpNode<'_>>> = &mut HashMap::new();
     let node_map = &mut HashMap::new();
@@ -246,40 +257,177 @@ pub fn part2(input: &InputType) -> String {
             dependency_lookup.insert(node.rhs, HashSet::from_iter([node]));
         }
     }
-    // println!("{:?}", dependency_lookup);
 
-    fn find_node<'a>(
-        input_a: &str,
-        input_b: &str,
-        dependency_lookup: &mut HashMap<&str, HashSet<&'a OpNode<'a>>>,
-    ) -> Option<Vec<&'a OpNode<'a>>> {
-        // println!("FInd node {} {}", input_a, input_b);
-        let deps_a = dependency_lookup.get(input_a)?;
-        let deps_b = dependency_lookup.get(input_b)?;
+    let mut bad_nodes = vec![];
 
-        let nodes = deps_a.intersection(deps_b).cloned().collect::<Vec<_>>();
-        if nodes.is_empty() {
-            println!("Failed to find node {}&{}: {:?}", input_a, input_b, nodes);
-            None
-        } else {
-            Some(nodes)
+    for node in nodes {
+        let deps = dependency_lookup.get(node.out);
+        match node.op {
+            Operator::And => {
+                // AND -> OR
+                if node.lhs == "x00" || node.rhs == "x00" {
+                    // This is fine
+                } else {
+                    if let Some(deps) = deps {
+                        if deps.len() == 1 {
+                            if deps.iter().next().unwrap().op != Operator::Or {
+                                println!("WRONG DEP OP {:?} {:?}", node, deps);
+                                bad_nodes.push(node.out);
+                            }
+                        } else {
+                            bad_nodes.push(node.out);
+                            println!("UNEXPECTED DEPS {:?} {:?}", node, deps);
+                        }
+                    } else {
+                        bad_nodes.push(node.out);
+                        println!("NO DEPS? {:?}", node);
+                    }
+                }
+            }
+            Operator::Or => {
+                // OR -> [XOR, AND]
+                if node.out == "z45" {
+                    // This is fine
+                } else {
+                    if let Some(deps) = deps {
+                        if deps.len() == 2 {
+                            if !deps.iter().any(|node| node.op == Operator::Xor)
+                                || !deps.iter().any(|node| node.op == Operator::And)
+                            {
+                                bad_nodes.push(node.out);
+                                println!("WRONG DEPS {:?} {:?}", node, deps);
+                            }
+                        } else {
+                            bad_nodes.push(node.out);
+                            println!("UNEXPECTED DEPS {:?} {:?}", node, deps);
+                        }
+                    } else {
+                        bad_nodes.push(node.out);
+                        println!("NO DEPS? {:?}", node);
+                    }
+                }
+            }
+            Operator::Xor => {
+                // XOR -> [XOR, AND]
+                // XOR -> [OUT]
+                if node.out.starts_with("z") {
+                    // Output node
+                } else {
+                    if let Some(deps) = deps {
+                        if deps.len() == 2 {
+                            if !deps.iter().any(|node| {
+                                node.op == Operator::Xor
+                                    && dependency_lookup[&node.out].out.starts_with("z")
+                            }) || !deps.iter().any(|node| node.op == Operator::And)
+                            {
+                                bad_nodes.push(node.out);
+                                println!("WRONG DEPS {:?} {:?}", node, deps);
+                            }
+                        } else {
+                            bad_nodes.push(node.out);
+                            println!("UNEXPECTED DEPS {:?} {:?}", node, deps);
+                        }
+                    } else {
+                        bad_nodes.push(node.out);
+                        println!("NO DEPS? {:?}", node);
+                    }
+                }
+            }
         }
     }
 
-    enum FindResult<'a> {
-        Err,
-        Partial(&'a OpNode<'a>, &'a str),
-        Ok(&'a OpNode<'a>),
-    }
+    // println!();
+    // println!();
+    println!();
+    println!();
+    println!();
+    println!("THE BADDIES {:?}", bad_nodes);
 
-    // fn find_node_op<'a>(
+    bad_nodes.sort();
+    bad_nodes.join(",")
+
+    // fn guess_node_op<'a>(
+    //     node: &'a OpNode<'a>,
+    //     dependency_lookup: &mut HashMap<&str, HashSet<&'a OpNode<'a>>>,
+    // ) {
+    //     // node
+    // }
+
+    // // println!("{:?}", dependency_lookup);
+    // for xor_node in nodes.iter().filter(|node| node.op == Operator::Xor) {
+    //     if (xor_node.lhs.starts_with('x') && xor_node.rhs.starts_with('y'))
+    //         || (xor_node.lhs.starts_with('y') && xor_node.rhs.starts_with('x'))
+    //             && xor_node.out != "z00"
+    //     {
+    //         // Node is half-sum, should output to full-sum (XOR) and carry-chain (AND)
+    //         if let Some(deps) = dependency_lookup.get(xor_node.out) {
+    //             if deps.iter().find(|node| node.op == Operator::Xor).is_none()
+    //                 || deps.iter().find(|node| node.op == Operator::And).is_none()
+    //             {
+    //                 println!("BAD ADDER OUT: {:?}", xor_node);
+    //             }
+    //         } else {
+    //             println!("BAD ADDER: {:?}", xor_node);
+    //         }
+    //     } else if !xor_node.out.starts_with('z') {
+    //         // Node is full-sum, should output to z
+    //         println!("BAD SUM OUT: {:?}", xor_node);
+    //     }
+    // }
+
+    // let fk = INPUT;
+
+    // // All XORs come from x+y _or_ out z
+    // for node in nodes
+    //     .iter()
+    //     .filter(|node| node.op != Operator::Xor && node.out.starts_with("z") && node.out != "z45")
+    // {
+    //     println!("BAD OUT {:?}", node);
+    // }
+
+    // for or_node in nodes.iter().filter(|node| node.op == Operator::Or) {
+    //     // Only carry
+    //     // Carry -> XOR -> ZOut
+    //     // AND & AND -> Carry
+
+    //     if let Some(deps) = dependency_lookup.get(or_node.out) {
+    //         if deps
+    //             .iter()
+    //             .find(|node| node.out.starts_with("z") && node.op == Operator::Xor)
+    //             .is_none()
+    //         {
+    //             println!("BAD CARRY {:?}", or_node);
+    //         }
+    //     } else {
+    //         println!("BAD CARRY OUT {:?}", or_node);
+    //     }
+    // }
+
+    // for and_node in nodes.iter().filter(|node| node.op == Operator::And) {
+    //     // Only outputs to OR
+    //     let deps = dependency_lookup.get(and_node.out).unwrap();
+    //     if !deps.iter().all(|node| node.op == Operator::Or) {
+    //         println!("BAD CARRY BLOCK {:?} {:?}", and_node, deps);
+    //     }
+    // }
+
+    // // println!("Swapped pairs");
+    // // for node in nodes.iter().filter(|node| {
+    // //     node.op == Operator::Xor
+    // //         && !node.out.starts_with("z")
+    // //         && !(node.lhs.starts_with('x') || node.rhs.starts_with('x'))
+    // // }) {
+    // //     println!("{:?}", node);
+    // // }
+
+    // fn find_node<'a>(
     //     input_a: &str,
     //     input_b: &str,
     //     dependency_lookup: &mut HashMap<&str, HashSet<&'a OpNode<'a>>>,
-    // ) -> FindResult<'a> {
+    // ) -> Option<Vec<&'a OpNode<'a>>> {
     //     // println!("FInd node {} {}", input_a, input_b);
-    //     let deps_a = dependency_lookup.get(input_a).unwrap();
-    //     let deps_b = dependency_lookup.get(input_b).unwrap();
+    //     let deps_a = dependency_lookup.get(input_a)?;
+    //     let deps_b = dependency_lookup.get(input_b)?;
 
     //     let nodes = deps_a.intersection(deps_b).cloned().collect::<Vec<_>>();
     //     if nodes.is_empty() {
@@ -290,125 +438,149 @@ pub fn part2(input: &InputType) -> String {
     //     }
     // }
 
-    let mut full_sum: Option<&OpNode<'_>> = None;
-    let mut full_carry: Option<&OpNode<'_>> = None;
+    // enum FindResult<'a> {
+    //     Err,
+    //     Partial(&'a OpNode<'a>, &'a str),
+    //     Ok(&'a OpNode<'a>),
+    // }
 
-    for node in find_node("x00", "y00", dependency_lookup).unwrap() {
-        if let Some(_dupe) = match node.op {
-            Operator::Xor => full_sum.replace(node),
-            Operator::And => full_carry.replace(node),
-            Operator::Or => {
-                panic!("Fuck")
-            }
-        } {
-            panic!("Fuck")
-        }
-    }
+    // // fn find_node_op<'a>(
+    // //     input_a: &str,
+    // //     input_b: &str,
+    // //     dependency_lookup: &mut HashMap<&str, HashSet<&'a OpNode<'a>>>,
+    // // ) -> FindResult<'a> {
+    // //     // println!("FInd node {} {}", input_a, input_b);
+    // //     let deps_a = dependency_lookup.get(input_a).unwrap();
+    // //     let deps_b = dependency_lookup.get(input_b).unwrap();
 
-    for bit in 1..45 {
-        let x_label = format!("x{:02}", bit);
-        let y_label = format!("y{:02}", bit);
+    // //     let nodes = deps_a.intersection(deps_b).cloned().collect::<Vec<_>>();
+    // //     if nodes.is_empty() {
+    // //         println!("Failed to find node {}&{}: {:?}", input_a, input_b, nodes);
+    // //         None
+    // //     } else {
+    // //         Some(nodes)
+    // //     }
+    // // }
 
-        print!(
-            "Bit {} A:{}, B:{}, carry-in:{} ",
-            bit,
-            x_label,
-            y_label,
-            full_carry.unwrap().out
-        );
+    // let mut full_sum: Option<&OpNode<'_>> = None;
+    // let mut full_carry: Option<&OpNode<'_>> = None;
 
-        let mut half_sum: Option<&OpNode<'_>> = None;
-        let mut half_carry: Option<&OpNode<'_>> = None;
-        let mut carry_chain: Option<&OpNode<'_>> = None;
+    // for node in find_node("x00", "y00", dependency_lookup).unwrap() {
+    //     if let Some(_dupe) = match node.op {
+    //         Operator::Xor => full_sum.replace(node),
+    //         Operator::And => full_carry.replace(node),
+    //         Operator::Or => {
+    //             panic!("Fuck")
+    //         }
+    //     } {
+    //         panic!("Fuck")
+    //     }
+    // }
 
-        // let x = dependency_lookup.get(&x_label);
-        // let y = dependency_lookup.get(&y_label);
+    // for bit in 1..45 {
+    //     let x_label = format!("x{:02}", bit);
+    //     let y_label = format!("y{:02}", bit);
 
-        for node in find_node(&x_label, &y_label, dependency_lookup).unwrap() {
-            if let Some(dupe) = match node.op {
-                Operator::Xor => half_sum.replace(node),
-                Operator::And => half_carry.replace(node),
-                Operator::Or => {
-                    panic!("Fuck")
-                }
-            } {
-                println!("Found duplicate for {}: {:?} and {:?}", bit, node, dupe);
-            }
-        }
+    //     print!(
+    //         "Bit {} A:{}, B:{}, carry-in:{} ",
+    //         bit,
+    //         x_label,
+    //         y_label,
+    //         full_carry.unwrap().out
+    //     );
 
-        for node in find_node(
-            half_sum.unwrap().out,
-            full_carry.unwrap().out,
-            dependency_lookup,
-        )
-        .unwrap()
-        {
-            match node.op {
-                Operator::And => carry_chain.replace(node),
-                Operator::Or => panic!("fuck"),
-                Operator::Xor => full_sum.replace(node),
-            };
-        }
+    //     let mut half_sum: Option<&OpNode<'_>> = None;
+    //     let mut half_carry: Option<&OpNode<'_>> = None;
+    //     let mut carry_chain: Option<&OpNode<'_>> = None;
 
-        full_carry.replace(
-            find_node(
-                half_carry.unwrap().out,
-                carry_chain.unwrap().out,
-                dependency_lookup,
-            )
-            .unwrap()
-            .first()
-            .unwrap(),
-        );
+    //     // let x = dependency_lookup.get(&x_label);
+    //     // let y = dependency_lookup.get(&y_label);
 
-        println!(
-            "half-sum:{}, full-sum:{}, half-carry:{}, carry-chair:{}, carry-out: {}",
-            half_sum.unwrap().out,
-            full_sum.unwrap().out,
-            half_carry.unwrap().out,
-            carry_chain.unwrap().out,
-            full_carry.unwrap().out,
-        );
-        // // Full sum is half sum + carry-in (the previous full-carry)
-        // full_sum.replace(
-        //     find_node(
-        //         half_sum.unwrap().out,
-        //         full_carry.unwrap().out,
-        //         dependency_lookup,
-        //     )
-        //     .iter()
-        //     .find(|node| node.op == Operator::And)
-        //     .unwrap(),
-        // );
+    //     for node in find_node(&x_label, &y_label, dependency_lookup).unwrap() {
+    //         if let Some(dupe) = match node.op {
+    //             Operator::Xor => half_sum.replace(node),
+    //             Operator::And => half_carry.replace(node),
+    //             Operator::Or => {
+    //                 panic!("Fuck")
+    //             }
+    //         } {
+    //             println!("Found duplicate for {}: {:?} and {:?}", bit, node, dupe);
+    //         }
+    //     }
 
-        // // Carry chain is half-sum and carry in
-        // carry_chain.replace(
-        //     find_node(
-        //         half_sum.unwrap().out,
-        //         full_carry.unwrap().out,
-        //         dependency_lookup,
-        //     )
-        //     .iter()
-        //     .find(|node| node.op == Operator::And)
-        //     .unwrap(),
-        // );
+    //     for node in find_node(
+    //         half_sum.unwrap().out,
+    //         full_carry.unwrap().out,
+    //         dependency_lookup,
+    //     )
+    //     .unwrap()
+    //     {
+    //         match node.op {
+    //             Operator::And => carry_chain.replace(node),
+    //             Operator::Or => panic!("fuck"),
+    //             Operator::Xor => full_sum.replace(node),
+    //         };
+    //     }
 
-        // let fk = find_node(
-        //     half_sum.unwrap().out,
-        //     half_carry.unwrap().out,
-        //     dependency_lookup,
-        // );
-        // if let Some(deps) = dependency_lookup.get(half_carry.unwrap().out) {
-        //     if deps.len() > 1 {
-        //         println!("{:?}", deps);
-        //     }
-        // } else {
-        //     println!("Missing full-carry bit{:02}", bit);
-        // }
-        // println!("{:?}", fk);
-        // let bit_sum =
-        // let bit_carry
-    }
+    //     full_carry.replace(
+    //         find_node(
+    //             half_carry.unwrap().out,
+    //             carry_chain.unwrap().out,
+    //             dependency_lookup,
+    //         )
+    //         .unwrap()
+    //         .first()
+    //         .unwrap(),
+    //     );
+
+    //     println!(
+    //         "half-sum:{}, full-sum:{}, half-carry:{}, carry-chair:{}, carry-out: {}",
+    //         half_sum.unwrap().out,
+    //         full_sum.unwrap().out,
+    //         half_carry.unwrap().out,
+    //         carry_chain.unwrap().out,
+    //         full_carry.unwrap().out,
+    //     );
+    // // Full sum is half sum + carry-in (the previous full-carry)
+    // full_sum.replace(
+    //     find_node(
+    //         half_sum.unwrap().out,
+    //         full_carry.unwrap().out,
+    //         dependency_lookup,
+    //     )
+    //     .iter()
+    //     .find(|node| node.op == Operator::And)
+    //     .unwrap(),
+    // );
+
+    // // Carry chain is half-sum and carry in
+    // carry_chain.replace(
+    //     find_node(
+    //         half_sum.unwrap().out,
+    //         full_carry.unwrap().out,
+    //         dependency_lookup,
+    //     )
+    //     .iter()
+    //     .find(|node| node.op == Operator::And)
+    //     .unwrap(),
+    // );
+
+    // let fk = find_node(
+    //     half_sum.unwrap().out,
+    //     half_carry.unwrap().out,
+    //     dependency_lookup,
+    // );
+    // if let Some(deps) = dependency_lookup.get(half_carry.unwrap().out) {
+    //     if deps.len() > 1 {
+    //         println!("{:?}", deps);
+    //     }
+    // } else {
+    //     println!("Missing full-carry bit{:02}", bit);
+    // }
+    // println!("{:?}", fk);
+    // let bit_sum =
+    // let bit_carry
+    // }
 
     // nodes.iter().flat_map(|node| [(node.lhs, node.out), (node.rhs,node.out)])
 
@@ -479,5 +651,5 @@ pub fn part2(input: &InputType) -> String {
     //     .collect::<HashMap<_, _>>();
     // println!("{:?}", node_solvers);
     // let fk = super::day24::INPUT;
-    String::new()
+    // String::new()
 }
