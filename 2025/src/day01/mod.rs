4d99cff9a40ce35210933883222fec3f06bb24dd
Cryowatt <Cryowatt@users.noreply.github.com>
2025-12-01 10:27:34 -0800

    2025 day 1 part 2

--- 2025/src/day01/mod.rs
+++ 2025/src/day01/mod.rs
@@ -1,17 +1,18 @@
 use advent::*;
+use num::Integer;
 
 advent_day!(Day01, parse, Vec<i32>, part1, part2);
 
-pub fn parse(input: &str) -> InputType {
+pub fn parse(input: &str) -> InputType<'_> {
     input
         .lines()
         .map(|line| {
             let tokens = line.split_at(1);
-            let direction = (match tokens.0 {
+            let direction = match tokens.0 {
                 "L" => -1,
                 "R" => 1,
                 _ => panic!(),
-            });
+            };
             direction * tokens.1.parse::<i32>().unwrap()
         })
         .collect()
@@ -39,26 +40,56 @@ pub fn part1(input: &InputType) -> usize {
         .scan(50, |state, rotation| {
             *state = (*state + rotation) % 100;
             Some(*state)
-            // match *state {
-            //     0 => Some(1),
-            //     _ => Some(0),
-            // }
         })
         .filter(|&state| state == 0)
         .count()
 }
 
-// /// ```rust
-// /// use advent_of_code_2025::day01::*;
-// /// let input = parse(
-// /// r"3   4
-// /// 4   3
-// /// 2   5
-// /// 1   3
-// /// 3   9
-// /// 3   3");
-// /// assert_eq!(31, part2(&input));
-// /// ```
+/// ```rust
+/// use advent_of_code_2025::day01::*;
+/// let input = parse(
+/// r"L68
+/// L30
+/// R48
+/// L5
+/// R60
+/// L55
+/// L1
+/// L99
+/// R14
+/// L82");
+/// assert_eq!(6, part2(&input));
+/// ```
+/// ```rust
+/// use advent_of_code_2025::day01::*;
+/// let input = parse(
+/// r"L50");
+/// assert_eq!(1, part2(&input));
+/// ```
+/// ```rust
+/// use advent_of_code_2025::day01::*;
+/// let input = parse(
+/// r"L150");
+/// assert_eq!(2, part2(&input));
+/// ```
 pub fn part2(input: &InputType) -> i32 {
-    i32::MIN
+    input
+        .iter()
+        .copied()
+        .scan(50, |state, rotation| {
+            let rotation_sign = rotation.signum();
+            let new_state = *state + rotation;
+            // Match zero crossings
+            let zc = match (state.signum(), new_state.signum()) {
+                (0, _) => 0,
+                (1, 1) => 0,
+                (-1, -1) => 0,
+                _ => 1,
+            };
+            // Count rotations and normalize position
+            let (q, r) = new_state.div_rem(&(100 * rotation_sign));
+            *state = r;
+            Some(q + zc)
+        })
+        .sum()
 }
