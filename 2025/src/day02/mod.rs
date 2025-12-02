def4baf468469505028179c61bb3d05c0da230fa
Cryowatt <Cryowatt@users.noreply.github.com>
2025-12-02 08:38:37 -0800

    Day 02 stubs

---
+++ 2025/src/day02/mod.rs
@@ -0,0 +1,57 @@
+use advent::*;
+
+advent_day!(Day02, parse, Vec<(u32, u32)>, part1, part2);
+
+pub fn parse(input: &str) -> InputType<'_> {
+    panic!()
+}
+
+/// ```rust
+/// use advent_of_code_2025::day01::*;
+/// let input = parse(
+/// r"L68
+/// L30
+/// R48
+/// L5
+/// R60
+/// L55
+/// L1
+/// L99
+/// R14
+/// L82");
+/// assert_eq!(3, part1(&input));
+/// ```
+pub fn part1(input: &InputType) -> usize {
+    panic!()
+}
+
+/// ```rust
+/// use advent_of_code_2025::day01::*;
+/// let input = parse(
+/// r"L68
+/// L30
+/// R48
+/// L5
+/// R60
+/// L55
+/// L1
+/// L99
+/// R14
+/// L82");
+/// assert_eq!(6, part2(&input));
+/// ```
+/// ```rust
+/// use advent_of_code_2025::day01::*;
+/// let input = parse(
+/// r"L50");
+/// assert_eq!(1, part2(&input));
+/// ```
+/// ```rust
+/// use advent_of_code_2025::day01::*;
+/// let input = parse(
+/// r"L150");
+/// assert_eq!(2, part2(&input));
+/// ```
+pub fn part2(input: &InputType) -> i32 {
+    panic!()
+}
--- 2025/src/main.rs
+++ 2025/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("AdventOfCode 2025");
 
     run_day!(1, day01::Day01);
-    // run_day!(2, day02::Day02);
+    run_day!(2, day02::Day02);
     // run_day!(3, day03::Day03);
     // run_day!(4, day04::Day04);
     // run_day!(5, day05::Day05);
